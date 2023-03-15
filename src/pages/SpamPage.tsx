236ab393a63df83af76a2353386f67e53376035b
kkpagaev <kkpagaev@gmail.com>
2023-03-15 07:08:46 +0200

    added list of targets

--- src/pages/SpamPage.tsx
+++ src/pages/SpamPage.tsx
@@ -1,29 +1,54 @@
+import { useEffect, useMemo, useState } from "react"
 import { useForm } from "react-hook-form"
-import { useNavigate } from "react-router-dom"
+import { useLocation, useNavigate } from "react-router-dom"
 import { apiCreateSpam, CreateSpamDto } from "../axios/api"
+import { useNotification } from "../context/NotificationContextProvider"
 import { useFetch } from "../hooks/useFetch"
 import { PaginationResponse } from "../types/pagination"
 import { Spam } from "../types/spam"
+import { Target } from "../types/target"
+
+function useQuery() {
+  const { search } = useLocation()
+
+  return useMemo(() => new URLSearchParams(search), [search])
+}
 
 export const SpamPage = () => {
   const { data } = useFetch<PaginationResponse<Spam>>({
     url: "/spam",
   })
+  const { data: targets } = useFetch<PaginationResponse<Target>>({
+    url: `/target?limit=1000`,
+  })
 
+  const query = useQuery()
+  const [selectedTargets, setSelectedTargets] = useState<number[]>([])
   const { register, handleSubmit, setValue } = useForm<CreateSpamDto>()
+  const { showNotification } = useNotification()
 
   const navigate = useNavigate()
   const onSubmit = async (dto: CreateSpamDto) => {
     const responce = await apiCreateSpam({
       title: dto.title,
       content: dto.content,
-      targets: [1, 2],
+      targets: selectedTargets,
     })
     if (responce.status === 200) {
       console.log("success")
+      showNotification({
+        title: "Success",
+        message: "Spam was created",
+        status: "success",
+      })
       navigate("/targets/")
     }
   }
+
+  useEffect(() => {
+    const tar = JSON.parse(query.get("targets") || "[]")
+    setSelectedTargets(tar)
+  }, [query])
 
   return (
     <div>
@@ -77,6 +102,52 @@ export const SpamPage = () => {
             />
           </div>
         </div>
+        <br />
+        <div className="list-group">
+          {selectedTargets.map((targetId) => {
+            const target = targets?.data.find(
+              (target) => target.id === targetId
+            )
+            return (
+              <div
+                key={targetId}
+                className="list-group-item d-flex justify-content-between align-items-center"
+              >
+                {target?.email}
+                <button
+                  type="button"
+                  className="btn-close"
+                  onClick={(e) => {
+                    e.preventDefault()
+                    setSelectedTargets(
+                      selectedTargets.filter((id) => id !== targetId)
+                    )
+                  }}
+                />
+              </div>
+            )
+          })}
+        </div>
+        <br />
+        <select
+          className="form-select"
+          aria-label="Default select example"
+          onChange={(e) => {
+            const targetId = parseInt(e.target.value)
+            if (targetId) {
+              setSelectedTargets([...selectedTargets, targetId])
+            }
+          }}
+        >
+          <option selected>Add target</option>
+          {targets?.data
+            .filter(({ id }) => !selectedTargets.includes(id))
+            .map((target) => (
+              <option key={target.id} value={target.id}>
+                {target.email}
+              </option>
+            ))}
+        </select>
         <br />
         <button type="submit" className="btn btn-primary">
           Submit
